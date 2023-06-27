d25acfb50809e954532746501f8cb94f5aa7a28f
Lisa Walters <lisacampiwalters@gmail.com>
2023-06-27 00:39:06 -0600

    added images and styles

--- app.js
+++ app.js
@@ -13,5 +13,26 @@ function PageTransitions(){
             this.className += ' active-btn';
         }) 
     }
+
+    //Sections Active Class
+
+    allSections.addEventListener('click', (e) => {
+        //whatever is clicked, target the dataset, and put that dataset into variable
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+            //hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }    
+    })
 }
 PageTransitions();
