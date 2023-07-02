1f090c4594e29a0656cec45327024b937adfa685
Lisa Walters <lisacampiwalters@gmail.com>
2023-07-02 17:17:04 -0600

    worked on darkLight

--- app.js
+++ app.js
@@ -34,5 +34,15 @@ function PageTransitions(){
             element.classList.add('active');
         }    
     })
+
+    // TOGGLE THEME===========================================================
+
+const themeBtn = document.querySelector('.theme-btn');
+themeBtn.addEventListener('click', () => {
+    let element = document.body;
+    element.classList.toggle('light-mode')
+})
 }
 PageTransitions();
+
+
