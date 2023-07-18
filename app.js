c4ae2cf11e7f6efa1ff6c86f37f9604cbd8dede8
Lisa Walters <lisacampiwalters@gmail.com>
2023-07-17 21:45:24 -0600

    added more aria labels

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ themeBtn.addEventListener('click', () => {
 
 // Surprise button===========================================================
 
-const clickOnButton = document.querySelector('.clickOnButton');
-const hiddenMessage = document.querySelector('.hiddenMessage');
-const open = document.querySelector('.open');
+let clickOnButton = document.querySelector('.clickOnButton');
+let hiddenMessage = document.querySelector('.hiddenMessage');
+let open = document.querySelector('.open');
 
 clickOnButton.addEventListener('click', () => {
     hiddenMessage.classList.toggle('surpriseMessage');
-    if (open.innerHTML == "Click for more information!"){
-    open.textContent ="Click to close";
+    if (open.innerHTML == "Click for more information!") {
+        open.textContent = "Click to close";
     } else {
         open.textContent = "Click for more information!";
     }
